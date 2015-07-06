5016d3c3327794054e0d3d32dee200a28cee4b5b
kirk <kirk.code@me.com>
2015-07-06 15:17:41 -0500

    Adding api path matching to middleware

--- lib/api-versions/middleware.rb
+++ lib/api-versions/middleware.rb
@@ -5,20 +5,21 @@ module ApiVersions
     end
 
     def call(env)
-      accept_string = env['HTTP_ACCEPT'] || ""
-      accepts = accept_string.split(',')
-      accepts.push("application/vnd.#{ApiVersions::VersionCheck.vendor_string}+json") unless accept_string.include?('application/vnd.')
-      offset = 0
-      accepts.dup.each_with_index do |accept, i|
-        accept.strip!
-        match = /\Aapplication\/vnd\.#{ApiVersions::VersionCheck.vendor_string}\s*\+\s*(?<format>\w+)\s*/.match(accept)
-        if match
-          accepts.insert i + offset, "application/#{match[:format]}"
-          offset += 1
-        end
-      end
-
+      if env['PATH_INFO'].match(/^\/(api)/)
+        accept_string = env['HTTP_ACCEPT'] || ""
+        accepts = accept_string.split(',')
+        accepts.push("application/vnd.#{ApiVersions::VersionCheck.vendor_string}+json") unless accept_string.include?('application/vnd.')
+        offset = 0
+        accepts.dup.each_with_index do |accept, i|
+          accept.strip!
+          match = /\Aapplication\/vnd\.#{ApiVersions::VersionCheck.vendor_string}\s*\+\s*(?<format>\w+)\s*/.match(accept)
+          if match
+            accepts.insert i + offset, "application/#{match[:format]}"
+            offset += 1
+          end # match if
+        end # accepts block
       env['HTTP_ACCEPT'] = accepts.join(',')
+      end # PATH_INFO match
       @app.call(env)
     end
   end
